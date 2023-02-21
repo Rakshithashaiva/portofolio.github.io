1547ec7cf8be8aa01e3544f3cdb9098523cbb0eb
rakshitha HR <rakshithahr39@gmail.com>
2023-02-21 21:45:39 +0530

    uppdated

---
+++ app.js
@@ -0,0 +1,48 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+function myFunction(){
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+  }
+
+
+  var messageArr = ["MERN Developer", "Frontend Developer", "Backend Developer"];
+  var textPosition = 0;
+  var speed = 200;
+
+  typewriter = () => {
+    // for(let i = 0; i < messageArr.length; i++) {
+    document.querySelector("#jobTitle").innerHTML = messageArr[0].substring(0, textPosition)  ;
+    if(textPosition ++  != messageArr[0].length)
+        setTimeout(typewriter, speed)
+  }
+
+
+  window.addEventListener("load" , typewriter);
+
+          
+   
